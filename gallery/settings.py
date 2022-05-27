b26c2c17570b8ed9ee50f8fee050618ee1a3ca98
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-27 21:22:58 +0300

    st:template-rendering - configuring project to render first template

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appgallery',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'dbgallery',
+        'USER':'moringa',
+        'PASSWORD':'Access',
+        'HOST':''
     }
 }
 
