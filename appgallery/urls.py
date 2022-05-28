acf6f6aadbdbb3e35521b94cca5a75345e5322e1
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-28 15:45:31 +0300

    ft:search functinality and location - adds functionality for search

--- appgallery/urls.py
+++ appgallery/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home,name='home'), 
-    path('search_results', views.search, name ='search_results') 
+    path('search_results/', views.search, name ='search_results') ,
+    # path('category_results/', views.search_category, name="category")
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- appgallery/views.py
+++ appgallery/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Image, Location, Category
 
 # Create your views here.
 def home(request):
-    all_images = Image.objects.all()
-    return render(request, 'index.html', {"templateimages":all_images})
+    # all_images = Image.objects.all()
+    categories=Category.objects.all()
+    locations=Location.objects.all
+    all_images=Image.get_images()
+    
+    if request.GET.get('location'):
+        all_images = Image.filter_by_location(request.GET.get('location'))
+        return render(request, 'location.html', {'images':all_images, 'categories':categories, 'location': request.GET.get('location')})
+    return render(request, 'index.html', {"templateimages":all_images, 'categorytemplates':categories,'localtemplates':locations })
 
 
 def search(request):
+    categories=Category.objects.all()
+    locations=Location.objects.all()
     if 'search' in request.GET and request.GET["search"]:
         term_of_search = request.GET.get("search")
         searched_images = Image.search_image(term_of_search)
+        print("___________________________________________")
         print(searched_images)
         message = f"{term_of_search}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+        return render(request, 'search.html',{"message":message,"images": searched_images, 'categories':categories, 'locations':locations})
 
     else:
         message = "seems you have not provided a search input"
         return render(request, 'search.html',{"message":message})
+    
+def location(request):
+    locations=Location.objects.all()
+    return render(request, 'location.html', {'locations':locations})
