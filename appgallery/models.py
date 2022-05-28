39bc1ae618dd143a3f2c7744314fa508cec8c39a
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-28 08:14:20 +0300

    ft:migrations - first migration

--- appgallery/models.py
+++ appgallery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # application models
 
 #location
-class Location(models.model):
+class Location(models.Model):
     location =models.CharField(max_length=200)   
 
 
@@ -12,8 +12,8 @@ class Location(models.model):
         
         
 #class Category 
-class Category(models.model):
-    category =models.CharField()
+class Category(models.Model):
+    category =models.CharField(max_length=200)
     
     
 
@@ -21,8 +21,13 @@ class Image(models.Model):
     image= models.ImageField(upload_to='photos/', null=True)
     name =models.CharField(max_length=100)
     description =models.TextField()
+    date_of_upload = models.DateTimeField(auto_now_add=True)
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        ordering=['date_of_upload']
     
     
     
--- appgallery/tests.py
+++ appgallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Location, Image, Category
 # Create your tests here.
+
+#Location Testcases
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location=Location(location = "Location1")
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
