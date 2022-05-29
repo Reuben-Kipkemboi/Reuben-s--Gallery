76d3f87185535d706c2b6449064c3f175c1c171e
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-29 23:01:09 +0300

    ft:copy-link-function - implements copy to clip board function

--- appgallery/static/Js/script.js
+++ appgallery/static/Js/script.js
@@ -3,4 +3,15 @@
       $('#myModal').on('shown.bs.modal', function () {
           $('#myInput').focus();
       });
-  });
+    });
+
+    function copyFunction() {
+        //  Get the text field by its id
+        let copiedLinkText = document.getElementById("input");
+
+        //   copy text inside the readonly input field
+        navigator.clipboard.writeText(copiedLinkText.value);
+      
+        /* Alert the copied text */
+        alert("Successfully copied the link.Now share your favourite image:" + copiedLinkText.value);
+      }
